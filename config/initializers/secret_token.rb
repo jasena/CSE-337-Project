e33665a1feb781700e61ed322f88d5d7cf1f81f8
Joe Sena <jasena@oakland.edu>
2013-03-12 19:24:14 -0400

    Adding rail files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSE337Project::Application.config.secret_token = '5dd01e6c3d0307f93f2732a4f5a5dafef0b3a917e6883ec6f376bc432c6579bd5f14c29b4ae41e42d4763cd53c65f8931958f94085c4a11f953537e0255bebd4'
