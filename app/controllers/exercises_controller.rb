cd7f63aaf2fc61eb413f13782a0adbf9963063c5
Joe Sena <jasena@oakland.edu>
2013-03-13 02:14:02 -0400

    Added update method

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,6 +4,14 @@ class ExercisesController < ApplicationController
   end
   
   def edit
-    @exercises = Exercise.find_by_id(1)
+    @exercises = Exercise.find_by_id(params[:id])
+  end
+  
+  def update
+    @exercises = Exercise.find(params[:id])
+    if @exercises.update_attributes(params[:exercises])
+      flash[:notice] = "Successfully updated workout titled: #{@exercises.name}"
+    end
+    redirect_to root_url
   end
 end
